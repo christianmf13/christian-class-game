fbb0a61192c037db45b86eb791a0a1654bbf572a
christianmf13 <christian.m.fuller13@gmail.com>
2026-02-02 15:28:47 -0800

    created second enemy and pickup item

--- scripts/boss_dino.gd
+++ scripts/boss_dino.gd
@@ -1,27 +1,26 @@
 extends Area2D
 
-const SPEED : float = 60.0
-var dir : int = -1
+const SPEED : float = 85.0
+var dir : int = 1
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var collision_shape_head_stomp: CollisionShape2D = $CollisionShapeHeadStomp
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
-@onready var killzone: Area2D = $Killzone
-
-
+@onready var head_stomp: CollisionShape2D = $HeadStomp
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
 		dir = -1
-		animated_sprite_2d.flip_h = false
+		animated_sprite_2d.flip_h = true
 	if ray_cast_left.is_colliding():
 		dir = 1
-		animated_sprite_2d.flip_h = true
+		animated_sprite_2d.flip_h = false
 	
 	position.x += SPEED * dir * delta
 
 #working on head stomp, not finished
+
+
+
 func _on_body_entered(body: Node2D) -> void:
-	($AnimatedSprite2D as AnimatedSprite2D).play("HeadStomp")
 	queue_free()
