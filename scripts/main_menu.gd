77231e71d2573b3a80d63e6b79b1b1de0794b724
christianmf13 <christian.m.fuller13@gmail.com>
2026-02-05 12:31:03 -0800

    menu + music
    added main menu w/ buttons and background music

---
+++ scripts/main_menu.gd
@@ -0,0 +1,14 @@
+extends CenterContainer
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_01.tscn")
+
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
